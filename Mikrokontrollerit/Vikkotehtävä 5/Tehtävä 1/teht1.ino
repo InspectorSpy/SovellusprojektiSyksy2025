86dd0f703e9c7b1f7513d50319cbab17df4ba706
InspectorSpy <103323455+InspectorSpy@users.noreply.github.com>
2025-11-07 20:42:31 +0200

    Upgrades people, upgrades.

---
+++ Mikrokontrollerit/Vikkotehtävä 5/Tehtävä 1/teht1.ino
@@ -0,0 +1,19 @@
+/*
+1: Tee sekuntikello käyttäen Loop-rakennetta ja Timeria. Sekuntikellon tulee laskea sekunnin kymmenyksiä, sekunteja ja minuutteja
+
+- Sekuntikello käynnistetään maadoittamalla pinni 2 (voit käyttää keskeytyksiä apuna)
+
+- Sekuntikello päivittää ajan sarjaportille sekunnin välein.
+
+- Sekuntikello pysäytetään maadoittamalla pinni 3, jolloin tulostetaan tarkka aika kymmenyksineen, ja nollataan aika uutta ajanottoa varten.
+*/
+
+#include <Arduino.h>
+
+void main() {
+
+}
+
+void loop() {
+
+}
