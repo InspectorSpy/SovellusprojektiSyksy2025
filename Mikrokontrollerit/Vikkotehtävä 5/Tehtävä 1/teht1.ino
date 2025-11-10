dbffd8a541a19ff0a2585571b1c598dd11850c17
InspectorSpy <103323455+InspectorSpy@users.noreply.github.com>
2025-11-10 14:47:09 +0200

    Upgrades people, upgrades.

--- Mikrokontrollerit/Vikkotehtävä 5/Tehtävä 1/teht1.ino
+++ Mikrokontrollerit/Vikkotehtävä 5/Tehtävä 1/teht1.ino
@@ -10,10 +10,81 @@
 
 #include <Arduino.h>
 
-void main() {
+uint8_t pinStart = 2;
+uint8_t pinStop = 3;
 
+volatile bool running = false;
+volatile bool shouldStop = false;
+
+unsigned long startTime = 0;
+unsigned long lastPrintTime = 0;
+
+unsigned int tenths = 0;
+unsigned int seconds = 0;
+unsigned int minutes = 0;
+
+void startTimer() {
+    if (!running) {
+        running  = true;
+        startTime = true;
+        lastPrintTime = millis();
+        Serial.println("Sekuntikello käynnistetty!");
+    }
+}
+
+void stopTimer() {
+    if (running) {
+        shouldStop = true;
+    }
+}
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(pinStart, INPUT_PULLUP);
+    pinMode(pinStop, INPUT_PULLUP);
+    
+    attachInterrupt(digitalPinToInterrupt(pinStart), startTimer, FALLING);
+    attachInterrupt(digitalPinToInterrupt(pinStop), stopTimer, FALLING);
+
+    Serial.println("Paina pinniä 2 käynnistääksesi");
 }
 
 void loop() {
+    if (running) {
+        unsigned long currentTime = millis();
+        unsigned long elapsed = currentTime - startTime;
 
+        tenths = (elapsed / 100) % 10;
+        seconds = (elapsed / 1000) % 60;
+        minutes = elapsed / 60000;
+
+        if (currentTime - lastPrintTime >= 1000) {
+            Serial.print("Aika: ");
+            Serial.print(minutes);
+            Serial.print(" min ");
+            Serial.print(seconds);
+            Serial.print(" s ");
+            Serial.println();
+            lastPrintTime = currentTime;
+        }
+        
+        if (shouldStop) {
+            Serial.print("Lopetettu aika: ");
+            Serial.print(minutes);
+            Serial.print(" min ");
+            Serial.print(seconds);
+            Serial.print(" s ");
+            Serial.print(tenths);
+            Serial.println(" kymmenesosaa");
+
+            running = false;
+            shouldStop = false;
+            tenths = 0;
+            seconds = 0;
+            minutes = 0;
+
+            Serial.println("Paina pinniä 2 käynnistääksesi uudelleen");
+            delay(500);
+        }
+    }
 }
